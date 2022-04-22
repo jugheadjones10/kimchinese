0733ca55bd9785d284f15039765262f59925e394
Kim Young Jin <kimyoungjin1001@gmail.com>
2022-04-22 07:00:52 +0000

    G

--- packages/backend/bull-config.js
+++ packages/backend/bull-config.js
@@ -1,8 +1,22 @@
+const url = require('url');
+
+let host = process.env.REDIS_TEST_HOST
+let port = 6379
+
+if(process.env.REDIS_URL){
+  const url = new URL(process.env.REDIS_URL)
+  host = url.host
+  port = url.port
+  console.log("redist", process.env.REDIS_URL)
+  console.log("redist host", host)
+  console.log("redist port", port)
+}
+
 exports.config = {
   concurrency: parseInt(process.env.QUEUE_CONCURRENCY || "1"),
   queueName: "mailbot",
   connection: {
-    host: process.env.NODE_ENV === "production" ? process.env.REDIS_HOST : process.env.REDIS_TEST_HOST,
-    port: parseInt(process.env.REDIS_PORT || "6379"),
+    host,
+    port
   }
 };
