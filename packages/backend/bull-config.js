a2ad7094cebdef7fa223aabf701b14d217522840
Kim Young Jin <kimyoungjin1001@gmail.com>
2022-04-23 06:23:33 +0000

    G

--- packages/backend/bull-config.js
+++ packages/backend/bull-config.js
@@ -7,7 +7,8 @@ let port = 6379
 deflogger.debug("REDIS url: ", process.env.REDIS_URL)
 if(process.env.REDIS_URL){
   const url = new URL(process.env.REDIS_URL)
-  host = url.hostname
+  // host = url.hostname
+  host = "redis://:p0b9018a1b837f4e9e5179cb08875565deb9b1889aea00904f0fdcc67313b02e2@ec2-18-214-181-102.compute-1.amazonaws.com"
   port = parseInt(url.port)
   console.log("redist", process.env.REDIS_URL)
   console.log("redist host", host)
