e43b4cac46de0ec3179067fc224c816062dbb4f3
Kim Young Jin <kimyoungjin1001@gmail.com>
2022-04-23 06:12:15 +0000

    G

--- packages/backend/bull-config.js
+++ packages/backend/bull-config.js
@@ -1,8 +1,10 @@
 const url = require('url');
+const { deflogger, imptlogger } = require("./logging.js")
 
 let host = process.env.REDIS_TEST_HOST
 let port = 6379
 
+deflogger.debug("REDIS url: ", process.env.REDIS_URL)
 if(process.env.REDIS_URL){
   const url = new URL(process.env.REDIS_URL)
   host = url.hostname
