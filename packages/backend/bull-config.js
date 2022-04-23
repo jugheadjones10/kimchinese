feec0df3be8f88077e8ac2df36e29b4978a0157d
Kim Young Jin <kimyoungjin1001@gmail.com>
2022-04-23 07:25:17 +0000

    G

--- packages/backend/bull-config.js
+++ packages/backend/bull-config.js
@@ -1,25 +1,18 @@
 const url = require('url');
 const { deflogger, imptlogger } = require("./src/logging.js")
 
-let host = process.env.REDIS_TEST_HOST
-let port = 6379
+let connection = {
+  host: process.env.REDIS_TEST_HOST,
+  port: 6379
+}
 
-deflogger.debug("REDIS url: ", process.env.REDIS_URL)
 if(process.env.REDIS_URL){
-  const url = new URL(process.env.REDIS_URL)
-  // host = url.hostname
-  host = "redis://:p0b9018a1b837f4e9e5179cb08875565deb9b1889aea00904f0fdcc67313b02e2@ec2-18-214-181-102.compute-1.amazonaws.com"
-  port = parseInt(url.port)
-  console.log("redist", process.env.REDIS_URL)
-  console.log("redist host", host)
-  console.log("redist port", port)
+  const IORedis = require("ioredis");
+  connection = new IORedis(process.env.REDIS_URL)
 }
 
 exports.config = {
   concurrency: parseInt(process.env.QUEUE_CONCURRENCY || "1"),
   queueName: "mailbot",
-  connection: {
-    host,
-    port
-  }
+  connection
 };
